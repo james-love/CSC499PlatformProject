ccdd3c98c16727b874165bb6a0a97463d734e89f
James-love <27236856+james-love@users.noreply.github.com>
2023-03-28 13:05:59 -0500

    redo level one with prefabs, add more one-way

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,6 +2,7 @@ public class Door : Interactable
 {
     public override void Interact()
     {
+        PlayerManager.Instance.SetInteractPopupVisibility(false);
         LevelManager.Instance.LoadInterior();
     }
 }
