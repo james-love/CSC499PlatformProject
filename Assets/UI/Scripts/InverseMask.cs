8862084212b9cb3e76a3ac611952d5432ed24f5b
James-love <27236856+james-love@users.noreply.github.com>
2023-03-28 17:31:38 -0500

    circle transition

--- Assets/UI/Scripts/InverseMask.cs
+++ Assets/UI/Scripts/InverseMask.cs
@@ -1,18 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class InverseMask : Image
 {
+    private Material inverseMaterial;
     public override Material materialForRendering
     {
         get
         {
-            Material material = new(base.materialForRendering);
-            material.SetInt("_StencilComp", (int)CompareFunction.NotEqual);
-            return material;
+            inverseMaterial ??= new(base.materialForRendering);
+            inverseMaterial.SetInt("_StencilComp", (int)CompareFunction.NotEqual);
+            return inverseMaterial;
         }
     }
 }
