3e4ffa8c380fd986cfcce70dff819a831792143d
James-love <27236856+james-love@users.noreply.github.com>
2023-03-30 10:32:59 -0500

    menu animations/graphics

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
 
-public class Parallax : MonoBehaviour {
-  public float scale;
-  public Transform tfPlayer;
-  private float origXPos;
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private float scale;
+    [SerializeField] private Transform tfPlayer;
+    private float origXPos;
+    private float playerOrigXPos;
 
-  private void Start() {
-    origXPos = transform.position.x;
-  }
+    private void Start()
+    {
+        origXPos = transform.position.x;
+        playerOrigXPos = tfPlayer.position.x;
+    }
 
-  private void LateUpdate() {
-    //transform.position = new Vector3((tfPlayer.position.x * scale), transform.position.y, transform.position.z);
-    transform.position = new Vector3(origXPos - (tfPlayer.position.x * scale), transform.position.y, transform.position.z);
-  }
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(origXPos - ((playerOrigXPos - tfPlayer.position.x) * scale), transform.position.y, transform.position.z);
+    }
 }
